587b523117d20a8c096e7e1a5b151fb129a15a9f
nischay <nischay.patel.code@gmail.com>
2021-12-12 13:59:34 +0530

    first commit

--- assignment_1_1.c
+++ assignment_1_1.c
@@ -4,6 +4,6 @@ int main()
     int a, b;
     printf("Enter the Numbers:");
     scanf("%d%d", &a, &b);
-    printf("Addition of %d and %d is %d", a, b, a + b);
+    printf("Addition of %d and %d is %d\n", a, b, a + b);
     return 0;
 }
